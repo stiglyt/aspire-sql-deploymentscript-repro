449c0bdf6b8244c56ab013ad27ef97d992f29327
Stig Lytjohan <stig.lytjohan@gmail.com>
2026-05-23 22:35:30 +0200

    removed frontend project

--- aspire-sql-deploy.AppHost/AppHost.cs
+++ aspire-sql-deploy.AppHost/AppHost.cs
@@ -2,8 +2,8 @@ using Aspire.Hosting.Azure;
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddAzureContainerAppEnvironment("app-env");
-var sqlScriptStorage = builder.AddAzureStorage("sql-script-storage");
-var appSql = builder.AddAzureSqlServer("sql");
+var sqlScriptStorage = builder.AddAzureStorage("sql-script-storage").RunAsEmulator();
+var appSql = builder.AddAzureSqlServer("sql").RunAsContainer();
   
 #pragma warning disable ASPIREAZURE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 appSql.WithAdminDeploymentScriptStorage(sqlScriptStorage);
@@ -14,11 +14,5 @@ var appDb = appSql.AddDatabase("db");
 var apiService = builder.AddProject<Projects.aspire_sql_deploy_ApiService>("apiservice")
     .WithHttpHealthCheck("/health")
     .WithReference(appDb);
-
-builder.AddProject<Projects.aspire_sql_deploy_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
 
 builder.Build().Run();
